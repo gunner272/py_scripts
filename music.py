de45bab2d8cbe67dbafffa1b1786c49e9cb2d7b3
rishabh <rishabh.sharma@students.iiit.ac.in>
2014-07-20 13:37:31 +0300

    improvements

--- music.py
+++ music.py
@@ -2,30 +2,36 @@ import os.path
 import os
 import sys
 import time
-ans="/media/g-drive/b2/songs"
-ans2="/media/g-drive/b2/isongs"
-l=[]
+
+songlist = []
+
 def filelsit(dirname):#list files in direc
 	return os.listdir(dirname)
-def printtr(dirname,lfile=[]):
-		for i in lfile:#recursiely check for each file in direc dirname
-			i=dirname+"/"+i
-			if(os.path.isdir(i)==True):#if file is direc
-					printtr(i,filelsit(i))
-			else:
-				i=i.lower()
-				l.append(i)
 
-printtr(ans,filelsit(ans))
-printtr(ans2,filelsit(ans2))
+
+def printtr(dirname, lfile = []):
+		for i in lfile:
+			i = dirname+"/"+i
+			if (os.path.isdir(i) == True):##recursively check for each file in direc dirname
+			    printtr(i, filelsit(i))
+			else:
+			    i = i.lower()
+			    songlist.append(i)
+
+
+for direc in sys.argv[1::]:
+	printtr(direc, filelsit(direc))
+
+
 while(1):
    try:
-	t=raw_input("enter filename ")
-	for i in l:
+	t = raw_input("enter songname ")
+	for i in songlist:
 		if t in i:
 			print i
    except KeyboardInterrupt:
-	    print "keyboard interupt"
+	    print "\nBye"
+	    time.sleep(1)
 	    os.system('clear')
 	    break
 
